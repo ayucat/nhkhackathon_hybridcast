9f169f80c2a7b68ba7f9a2375a417a0f398036b0
Lewuathe <lewuathe@me.com>
2014-05-24 19:31:44 +0900

    Focus capture

--- newsmile/js/main.js
+++ newsmile/js/main.js
@@ -1,3 +1,5 @@
+var focus = 1;
+
 function debug(str) {
     var o = JSON.stringify(str);
     $("#debug").html(o);
@@ -5,14 +7,21 @@ function debug(str) {
 
 $(function() {
     document.addEventListener("keydown", eventKey, false);
+    $("div#cap" + focus).addClass("email-item-selected");
 
     function eventKey(e) {
         var key = e.keyCode;
         debug(key);
         
+        $("div#cap" + focus).removeClass("email-item-selected");
         if(key == VK_DBUTTON) {
             //alert("VK_DBUTTON");
             navigator.applicationManager.launchDataBroadcastingBrowser({});
+        } else if (key == VK_UP) {
+            focus--;
+        } else if(key == VK_DOWN) {
+            focus++;
         }
+        $("div#cap" + focus).addClass("email-item-selected");
     }
 });
