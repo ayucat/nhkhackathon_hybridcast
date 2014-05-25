3dde02ed6e7ccd73958c98834e849221609aa113
Lewuathe <lewuathe@me.com>
2014-05-25 14:23:36 +0900

    Update

--- newsmile/js/main.js
+++ newsmile/js/main.js
@@ -18,30 +18,29 @@ function makeAgeString(age) {
 $(function() {
     function getCapList(data) {
         for (var i = 0; i < data.length; i++) {
-            var $item = $("<div/>").addClass("email-item").addClass("email-item-selected").addClass("pure-g").attr("id", "cap" + i);
-            var $avatar = $("<div/>").addClass("pure-u");
-            var $img = $("<img/>").addClass("email-avatar").attr("src", data[i]["image_url"]);
+            var $item = $("<div/>").addClass("email-item").addClass("pure-g").attr("id", "cap" + i);
+            var $avatar = $("<div/>").addClass("pure-u").css("height", "140px");
+            var $img = $("<img/>").addClass("email-avatar").attr("id", "cap" + i + "_image").attr("src", data[i]["image_url"]);
 
-            var $news = $("<div/>").addClass("pure-u");
+            var $news = $("<div/>").addClass("pure-u").css("height", "80px");
             var $face_icon = $("<div/>").addClass("face_icon");
             var $face_img = $("<img/>").addClass("email-avatar_icon").attr("src", "img/common/face0" + data[i]["emotion"] + ".png");
             
             var $news_set = $("<div/>").addClass("news_set");
             var $news_date = $("<h4/>").addClass("news_date").text(data[i]["date"] + "国際報道2014");
-            var $news_link = $("<p/>").addClass("news_link").html("<a href=\"http://bit.ly/1k4zQ5s\">http://bit.ly/1k4zQ5s</a>");
-            var $news_age = $("<p/>").addClass("news_age").text(makeAgeString(data[i]["age"]));
 
-            var $btn_set = $("<div/>").addClass("btn_set");
-            var $map_anchor = $("<a/>").attr("href", "#").html("<img alt=\"\" src=\"img/common/map_link.png\">");
-            var $delete_anchor = $("<a/>").attr("href", "#").html("<img src=\"img/common/icon_delete.png\" alt=\"\" width=\"40\" height=\"19\">");
-            $btn_set.append($map_anchor).append($delete_anchor);
+            var $btn_set = $("<div/>").addClass("btn_set").css("width", "250px");
+            var $blue_anchor = $("<a/>").attr("href", "#").html("<img alt=\"\" src=\"img/common/btn_blue.png\">");
+            var $red_anchor = $("<a/>").attr("href", "#").html("<img src=\"img/common/btn_red.png\" alt=\"\">");
+            var $yellow_anchor = $("<a/>").attr("href", "#").html("<img src=\"img/common/btn_green.png\" alt=\"\">");
+            $btn_set.append($blue_anchor).append($red_anchor).append($yellow_anchor);
             
             $avatar.append($img);
 
             $face_icon.append($face_img);
             $news.append($face_icon);
             
-            $news_set.append($news_date).append($news_link).append($news_age).append($btn_set);
+            $news_set.append($news_date).append($btn_set);
             $news.append($news_set);
             
             $item.append($avatar);
@@ -55,9 +54,13 @@ $(function() {
         $(".email-count").text("(" + capLength +")")
     }
 
-    $("#dialog").dialog({position:{my:"left-100 top+200", of: "body"}});
-    //$("#dialog").dialog();
+    function startAnimation(cap_id) {
+        var image_url = $("#" + cap_id).attr("src");
+        
+    }
 
+    $("#dialog").dialog();
+    
     document.addEventListener("keydown", eventKey, false);
     $("div#cap" + focus).addClass("email-item-selected");
 
@@ -73,7 +76,7 @@ $(function() {
                 },
                 "date": "2014-05-25",
                 "desc": "This is text subject 1",
-                "image_url": "http://211.129.73.42:21080/s2ia/s1/latest"
+                "image_url": "http://211.129.73.42:21080/s2ia/s1/2014-05-16T22:00:02"
             },
             {
                 "emotion": "2",
@@ -84,7 +87,7 @@ $(function() {
                 },
                 "date": "2014-05-25",
                 "desc": "This is text subject 2",
-                "image_url": "http://211.129.73.42:21080/s2ia/g1/latest"
+                "image_url": "http://211.129.73.42:21080/s2ia/s1/2014-05-16T21:00:01"
             },
             {
                 "emotion": "3",
@@ -95,7 +98,7 @@ $(function() {
                 },
                 "date": "2014-05-25",
                 "desc": "This is text subject 1",
-                "image_url": "http://211.129.73.42:21080/s2ia/s3/latest"
+                "image_url": "http://211.129.73.42:21080/s2ia/s1/2014-05-16T21:59:50"
             },
             {
                 "emotion": "4",
@@ -106,7 +109,7 @@ $(function() {
                 },
                 "date": "2014-05-25",
                 "desc": "This is text subject 1",
-                "image_url": "http://211.129.73.42:21080/s2ia/e1/latest"
+                "image_url": "http://211.129.73.42:21080/s2ia/s1/2014-05-16T21:59:58"
             },
             {
                 "emotion": "5",
@@ -117,7 +120,7 @@ $(function() {
                 },
                 "date": "2014-05-25",
                 "desc": "This is text subject 1",
-                "image_url": "http://211.129.73.42:21080/s2ia/s1/latest"
+                "image_url": "http://211.129.73.42:21080/s2ia/s1/2014-05-16T21:59:39"
             }
         ];
         getCapList(data);
@@ -142,6 +145,8 @@ $(function() {
                 focus = 0;
                 $('#list').animate({ scrollTop: 0 }, 'fast');
             }
+        } else if(key == VK_ENTER) {
+            $("#dialog").dialog();
         }
         $("div#cap" + focus).addClass("email-item-selected");
     }
